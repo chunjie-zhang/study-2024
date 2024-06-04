f41c8744805e434f1b659cb030f74d964a2abac0
zhangchunjie8 <zhangchunjie8@jd.com>
2024-06-04 10:37:00 +0800

    feat: electron学习day02

--- study-electron/day02/src/electron/preload.js
+++ study-electron/day02/src/electron/preload.js
@@ -1,3 +1,11 @@
-const { ipcRenderer } = require("electron");
+const { ipcRenderer, shell } = require("electron");
+const fs = require('fs')
+
+// shell.openExternal('https://jd.com');
 
 ipcRenderer.send('init', 'ok')
+
+window.api = {
+  a: 1
+}
+fs.writeFileSync('a.txt', 'aaaa')
--- study-electron/day02/src/electron/window.js
+++ study-electron/day02/src/electron/window.js
@@ -1,4 +1,4 @@
-const { BrowserWindow } = require('electron');
+const { BrowserWindow, screen } = require('electron');
 const path = require("path")
 
 const createWindow = () => {
@@ -8,13 +8,21 @@ const createWindow = () => {
     alwaysOnTop: true,
     frame: true,
     webPreferences: {
-      preload: path.resolve(__dirname, 'preload.js')
+      preload: path.resolve(__dirname, 'preload.js'),
+      // contextIsolation: false, // 预加载脚本不隔离
+      // nodeIntegration: true, // 开启node
+      sandbox: false,
     }
   })
 
   mainWindow.webContents.openDevTools();
 
   mainWindow.loadFile(path.resolve(__dirname, '../web/index.html'));
+  setTimeout(() => {
+    // mainWindow.center();
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+    mainWindow.setBounds({width: 850, height: 850, x: width / 2 - 425, y: height / 2 - 425}, true);
+  }, 1000)
 }
 
 module.exports = {
